1ab5e867d62caa69c8574f03b199ae9d43b73fe0
Arun <Arun@DESKTOP-051EIJR>
2023-10-31 12:29:28 +0530

    junken sample program

---
+++ junken_sample/src/main/java/junken_samp_pgm/JunkenSampPgm.java
@@ -0,0 +1,14 @@
+package junken_samp_pgm;
+
+public class JunkenSampPgm {
+
+	
+		// TODO Auto-generated method stub
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
